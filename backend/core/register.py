cbc8e29d95fbe99ca9dba51ad0e090749779bfa1
CodarZ <1476712061@qq.com>
2025-01-21 21:33:59 +0800

    update register.py
    - FastAPI Limiter

--- backend/core/register.py
+++ backend/core/register.py
@@ -4,8 +4,10 @@ import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi_limiter import FastAPILimiter
 
 from backend.common.logger import register_logger
+from backend.common.response.check import http_limit_callback
 from backend.core.config import settings
 from backend.core.paths import STATIC_DIR
 from backend.database.mysql import create_table
@@ -18,11 +20,18 @@ async def register_lifespan(_: FastAPI):
     await create_table()
     # 连接 redis
     await redis_client.open()
-
+    # 初始化 limiter
+    await FastAPILimiter.init(
+        redis=redis_client,
+        prefix=settings.REQUEST_LIMITER_REDIS_PREFIX,
+        http_callback=http_limit_callback,
+    )
     yield
 
     # 关闭 redis 连接
     await redis_client.close()
+    # 关闭 limiter
+    await FastAPILimiter.close()
 
 
 def register_app():
