40c83ce8051f2d5d0d0dcb146b901ff26d5d3c5b
CodarZ <1476712061@qq.com>
2025-01-21 15:37:07 +0800

    register static file
    注册开发环境下，静态文件访问服务

--- backend/core/register.py
+++ backend/core/register.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
 from backend.common.logger import register_logger
 from backend.core.config import settings
+from backend.core.paths import STATIC_DIR
 
 
 @asynccontextmanager
@@ -29,7 +31,7 @@ def register_app():
     register_logger()
     #
     # # 静态文件
-    # register_static_file(app)
+    register_static_file(app)
     #
     # # 中间件
     # register_middleware(app)
@@ -44,3 +46,16 @@ def register_app():
     # register_exception(app)
 
     return app
+
+
+def register_static_file(app: FastAPI):
+    """
+    静态文件交互开发模式, 生产不要开启
+    """
+    if settings.FASTAPI_STATIC_FILES:
+        from fastapi.staticfiles import StaticFiles
+
+        if not os.path.exists(STATIC_DIR):
+            os.makedirs(STATIC_DIR)
+
+        app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
