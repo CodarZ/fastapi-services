55616ca6be18b9784635e4ca50b28d1542233beb
CodarZ <1476712061@qq.com>
2025-01-22 16:18:39 +0800

    update dataclasses.py
    快速定义只包含数据属性的类

--- backend/common/dataclasses.py
+++ backend/common/dataclasses.py
@@ -1,2 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import dataclasses
+from datetime import datetime
+
+from fastapi import Response
+
+from backend.common.enums import StatusEnum
+
+
+@dataclasses.dataclass
+class IpInfo:
+    ip: str                             # 原始 IP 地址
+    country: str | None                 # 国家
+    region: str | None                  # 省/州
+    city: str | None                    # 城市
+    district: str | None                # 区
+    postal_code: str | None             # 邮政编码
+    timezone: str | None                # 时区
+    latitude: float | None              # 纬度
+    longitude: float | None             # 经度
+    location_code: str | None           # 位置编码，例如北京为 110000
+    full_address: str | None            # 完整地址（例如：北京市朝阳区三里屯）
+
+
+@dataclasses.dataclass
+class UserAgentInfo:
+    user_agent: str                       # 原始的 User-Agent 字符串
+    os: str | None                        # 操作系统（如 Windows, macOS, iOS, Android）
+    os_version: str | None                # 操作系统版本（如 10.0.1, Big Sur, etc.）
+    browser: str | None                   # 浏览器类型（如 Chrome, Safari, Firefox）
+    browser_version: str | None           # 浏览器版本（如 91.0.4472.124）
+    device: str | None                    # 设备类型（如 Mobile, Tablet, Desktop）
+    device_model: str | None              # 具体的设备型号（如 iPhone 12, Galaxy S21）
+
+
+@dataclasses.dataclass
+class RequestCallNext:
+    code: str
+    msg: str
+    status: StatusEnum
+    err: Exception | None
+    response: Response
+
+
+@dataclasses.dataclass
+class NewToken:
+    new_access_token: str
+    new_access_token_expire_time: datetime
+    new_refresh_token: str
+    new_refresh_token_expire_time: datetime
+
+
+@dataclasses.dataclass
+class AccessToken:
+    access_token: str
+    access_token_expire_time: datetime
+
+
+@dataclasses.dataclass
+class RefreshToken:
+    refresh_token: str
+    refresh_token_expire_time: datetime
