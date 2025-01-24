15f22f6d468fe3775532cf8c97c1ad7a2f6579a7
CodarZ <1476712061@qq.com>
2025-01-24 11:39:26 +0800

    update handler.py
    - 错误信息优先返回自定义验证信息

--- backend/common/exception/handler.py
+++ backend/common/exception/handler.py
@@ -45,9 +45,7 @@ def _get_exception_code(status_code: int):
     return code
 
 
-async def _validation_exception_handler(
-    request: Request, e: RequestValidationError | ValidationError
-):
+async def _validation_exception_handler(request: Request, e: RequestValidationError | ValidationError):
     """
     数据验证异常处理，返回统一的响应结构。
 
@@ -68,34 +66,27 @@ async def _validation_exception_handler(
                 ctx_error = ctx.get("error")
                 if ctx_error:
                     error["ctx"]["error"] = (
-                        ctx_error.__str__().replace("'", '"')
-                        if isinstance(ctx_error, Exception)
-                        else None
+                        ctx_error.__str__().replace("'", '"') if isinstance(ctx_error, Exception) else None
                     )
         errors.append(error)
     error = errors[0]  # 只返回第一个错误信息
+
     if error.get("type") == "json_invalid":
         message = "json解析失败"
     else:
         # 拼接错误详细信息
         error_input = error.get("input")
         field = str(error.get("loc")[-1])
-        error_msg = error.get("msg")
-        message = (
-            f"{field} {error_msg}，输入：{error_input}"
-            if settings.ENVIRONMENT == "development"
-            else error_msg
-        )
-    msg = f"请求参数非法: {message}"
+        error_msg = error.get("ctx", {}).get("error", error.get("msg"))
+        message = f"{field} {error_msg}，输入：{error_input}" if settings.ENVIRONMENT == "development" else error_msg
+    msg = f"{CustomResponseCode.HTTP_422.msg}: {message}"
     data = {"errors": errors} if settings.ENVIRONMENT == "development" else None
     content = {
         "code": StandardResponseCode.HTTP_422,
         "msg": msg,
         "data": data,
     }
-    request.state.__request_validation_exception__ = (
-        content  # 用于在中间件中获取异常信息
-    )
+    request.state.__request_validation_exception__ = content  # 用于在中间件中获取异常信息
     content.update(trace_id=get_request_trace_id(request))  # 添加请求唯一标识
     return MsgSpecJSONResponse(status_code=422, content=content)
 
@@ -122,16 +113,12 @@ def register_exception(app: FastAPI):
         )
 
     @app.exception_handler(RequestValidationError)
-    async def fastapi_validation_exception_handler(
-        request: Request, exc: RequestValidationError
-    ):
+    async def fastapi_validation_exception_handler(request: Request, exc: RequestValidationError):
         """Fastapi 数据验证异常处理"""
         return await _validation_exception_handler(request, exc)
 
     @app.exception_handler(ValidationError)
-    async def pydantic_validation_exception_handler(
-        request: Request, exc: ValidationError
-    ):
+    async def pydantic_validation_exception_handler(request: Request, exc: ValidationError):
         """pydantic 数据验证异常处理"""
         return await _validation_exception_handler(request, exc)
 
@@ -233,15 +220,9 @@ def register_exception(app: FastAPI):
             request.state.__request_cors_500_exception__ = content
             content.update(trace_id=get_request_trace_id(request))
             response = MsgSpecJSONResponse(
-                status_code=(
-                    exc.code
-                    if isinstance(exc, BaseExceptionMixin)
-                    else StandardResponseCode.HTTP_500
-                ),
+                status_code=(exc.code if isinstance(exc, BaseExceptionMixin) else StandardResponseCode.HTTP_500),
                 content=content,
-                background=(
-                    exc.background if isinstance(exc, BaseExceptionMixin) else None
-                ),
+                background=(exc.background if isinstance(exc, BaseExceptionMixin) else None),
             )
             origin = request.headers.get("origin")
             if origin:
@@ -257,9 +238,7 @@ def register_exception(app: FastAPI):
                 has_cookie = "cookie" in request.headers
                 if cors.allow_all_origins and has_cookie:
                     response.headers["Access-Control-Allow-Origin"] = origin
-                elif not cors.allow_all_origins and cors.is_allowed_origin(
-                    origin=origin
-                ):
+                elif not cors.allow_all_origins and cors.is_allowed_origin(origin=origin):
                     response.headers["Access-Control-Allow-Origin"] = origin
                     response.headers.add_vary_header("Origin")
             return response
