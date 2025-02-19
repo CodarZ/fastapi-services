d1b13fac1c1b1f74438d6b6b4f47a92de546ee74
CodarZ <1476712061@qq.com>
2025-02-19 13:29:44 +0800

    chore: 删除 auth 部分, 计划重写.

--- backend/app/admin/api/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from fastapi import APIRouter
-
-from .auth import router as auth_router
-
-router = APIRouter(prefix="/auth", tags=["认证授权"])
-
-router.include_router(auth_router)
--- backend/app/admin/api/router.py
+++ backend/app/admin/api/router.py
@@ -3,10 +3,8 @@
 
 from fastapi import APIRouter
 
-from .auth import router as auth_router
 from .sys import router as sys_router
 
 admin_router = APIRouter()
 
 admin_router.include_router(sys_router)
-admin_router.include_router(auth_router)
